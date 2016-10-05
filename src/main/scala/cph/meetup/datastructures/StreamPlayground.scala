824d45a0f40dfe7a07186c652d47a4c54e965a52
ailveen <ailveen@yahoo.com>
2016-10-05 23:22:25 +0200

    more examples

--- src/main/scala/cph/meetup/datastructures/StreamPlayground.scala
+++ src/main/scala/cph/meetup/datastructures/StreamPlayground.scala
@@ -1,5 +1,15 @@
 package cph.meetup.datastructures
 
-class StreamPlayground extends App {
+object StreamPlayground extends App {
 
+  def fib(n: Long): Long = {
+    @annotation.tailrec
+    def loop(n: Long, prev: Long, cur: Long): Long =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(n, 0, 1)
+  }
+
+  // stream of infinite fibonaccis
+  def fibStream(n: Long): Stream[Long] = ???
 }
